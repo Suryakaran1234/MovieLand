d52a0d058554fbaa10a1bab0046f41b2edfad5ac
Surya Karan Sharma <suryakaran1234@gmail.com>
2022-03-05 13:42:43 +0530

    Completed this crash course with MovieLand Project

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+// 68fe8ac3
+
+const API_URL = 'http://www.omdbapi.com?apikey=761c4dc2';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('spiderman');
+  }, []);
+
+  return (
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input
+          type='text'
+          placeholder='Search for Movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie, index) => (
+            <MovieCard movie={movie} key={index} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
